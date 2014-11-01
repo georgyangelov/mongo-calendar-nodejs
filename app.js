c269de54fb9fdf672cecc7509ff6feeba736c3d0
Georgy Angelov <georgyangelov@gmail.com>
2014-11-02 00:38:30 +0200

    Express project skeleton

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var users = require('./routes/users');
+
+var app = express();
+
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: err
+    });
+});
+
+module.exports = app;
